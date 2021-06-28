0dc446b9eb5dd8c9fb485ae32c9dde685a63de7e
Mikhail Muchko <muchkom@gmail.com>
2021-06-28 20:57:52 +0300

    fix /today output format

--- handlers/today.py
+++ handlers/today.py
@@ -1,3 +1,5 @@
+import datetime
+
 from telegram import Update
 from telegram.ext import CommandHandler, CallbackContext, Updater
 
@@ -7,12 +9,20 @@ from db import DBConnector
 def today(update: Update, context: CallbackContext):
     db_connect: DBConnector = context.bot_data['db_connect']
     result = db_connect.users_products.select_today(update.effective_user.id)
+    user = db_connect.users.select_id(update.effective_user.id)
     reply_str = ''
-    for row in result:
-        # todo format row
-        reply_str += '{}\n'.format(row)
+    today_total = 0
+    for i, row in enumerate(result):
+        name = row['name']
+        total_kcal = row['total_kcal']
+        time: datetime.datetime = row['time']
+        reply_str += f'{i + 1}. Product: {name}. Kcal: {total_kcal:.0f}. Time: {time.time()}\n'
 
-    update.message.reply_text(reply_str or "Nothing was eaten today!")
+        today_total += row['total_kcal']
+
+    update.message.reply_text(reply_str and (f'{reply_str}'
+                                             f'Today kcal eaten: {today_total:.0f} '
+                                             f'Max allowed: {user["kcal"]:.0f}') or "Nothing was eaten today!")
 
 
 def add_handler(updater: Updater):
